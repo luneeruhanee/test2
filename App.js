f9f593273aec6dc10eefea05adf8bd3a3503ad66
luneeruhanee <47787422+luneeruhanee@users.noreply.github.com>
2019-02-26 12:05:21 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Ruhanee Tohlee 1412682!</Text>
       </View>
     );
   }
